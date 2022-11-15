0964ee3706d655feba1d75196c15b1868830d6c7
alam21022022 <sajjad.alam@fissionlabs.com>
2022-11-15 14:19:07 +0530

    added IT page

--- src/pages/IT/IT.jsx
+++ src/pages/IT/IT.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import IndividualDepartmentDetails from "../../components/IndividualDepartmentDetails/IndividualDepartmentDetails";
 
 const IT = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -11,6 +12,7 @@ const IT = () => {
     <div className="no-shadow">
       <div className="col-md-8 col-sm-8 m-auto">
         <h5 className='text-center bold-text mt-3 mb-4'>This is IT page</h5>
+        <IndividualDepartmentDetails departmentName={'IT'} />
       </div>
     </div>
   );
