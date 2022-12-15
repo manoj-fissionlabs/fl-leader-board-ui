5a566c8eb4dd400f3fe0ae5e8e260e12c533fdd1
Arpit-FL <arpit.sharma@fissionlabs.com>
2022-12-15 15:51:28 +0530

    fixed alignment issue added total count

--- src/pages/Backend/Backend.jsx
+++ src/pages/Backend/Backend.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import IndividualDepartmentDetails from "../../components/IndividualDepartmentDetails/IndividualDepartmentDetails";
 
 const Backend = () => {
@@ -7,10 +8,12 @@ const Backend = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  const {totalMemberCount} = useSelector(state => state.data);
+  
   return (
     <div className="no-shadow mb-5">
       <div className="col-md-10 col-sm-10 m-auto">
-        <h5 className='text-center bold-text mt-3 mb-4'>This is Backend page</h5>
+        <h5 className='text-center bold-text mt-3 mb-4'>Backend - {totalMemberCount}</h5>
         <IndividualDepartmentDetails departmentName={'backendTechnologies'} />
       </div>
     </div>
--- src/pages/IT/IT.jsx
+++ src/pages/IT/IT.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import IndividualDepartmentDetails from "../../components/IndividualDepartmentDetails/IndividualDepartmentDetails";
 
 const IT = () => {
@@ -7,11 +8,12 @@ const IT = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const {totalMemberCount} = useSelector(state => state.data);
 
   return (
     <div className="no-shadow">
       <div className="col-md-10 col-sm-10 m-auto">
-        <h5 className='text-center bold-text mt-3 mb-4'>This is IT page</h5>
+        <h5 className='text-center bold-text mt-3 mb-4'>IT - {totalMemberCount}</h5>
         <IndividualDepartmentDetails departmentName={'IT'} />
       </div>
     </div>
